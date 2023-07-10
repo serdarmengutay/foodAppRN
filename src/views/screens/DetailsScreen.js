595e2925fd805ad907cc0eb529b18631ae403722
Serdar Mengütay <serdarmngty03@gmail.com>
2023-07-10 10:53:27 +0300

    foodAppRN

---
+++ src/views/screens/DetailsScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SafeAreaView, View,Text } from 'react-native'
+const DetailsScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Details</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default DetailsScreen
---
+++ src/views/screens/OnBoardScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SafeAreaView, View,Text } from 'react-native'
+const OnBoardScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>On board</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default OnBoardScreen
